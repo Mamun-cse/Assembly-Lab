b1a136a3aef927416a216fe978548f453b3b4607
Mamun-cse <37404399+Mamun-cse@users.noreply.github.com>
2020-02-25 23:38:58 +0600

    Seris sum

---
+++ SERISSUM.asm
@@ -0,0 +1,57 @@
+INCLUDE 'EMU8086.INC'
+.MODEL SMALL
+.STACK 100H
+.DATA
+N DB ?
+FACT DB ?
+.CODE
+MOV AX,@DATA
+MOV DS,AX
+
+MAIN PROC
+    MOV AH,1
+    INT 21H
+    
+    SUB AL,48
+    
+    CMP AL,0
+    JE IS_ZERO 
+    
+    JMP NOT_ZERO
+    
+    IS_ZERO:
+    MOV AL,1
+    JMP ENDFOR
+    
+    NOT_ZERO:
+    MOV N,AL
+    DEC N
+    
+    FOR:
+    CMP N,0
+    JE ENDFOR
+    MUL N
+    DEC N
+    JMP FOR
+    
+    ENDFOR:
+    MOV FACT,AL
+    ADD FACT,48
+    
+    
+    MOV AH,2
+    MOV DL,0AH
+    INT 21H
+    MOV DL,0DH
+    INT 21H
+    
+    PRINT 'FACTORIAL :'
+    MOV AH,2
+    MOV DL,FACT
+    INT 21H
+    
+    MOV AH,4CH
+    INT 21H
+    
+    MAIN ENDP
+END MAIN
