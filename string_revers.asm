8d6bcf54a40f1d9fb187d50c4bb0a199d89aafb1
Mamun-cse <37404399+Mamun-cse@users.noreply.github.com>
2020-03-02 18:04:36 +0600

    Update string_revers.asm

--- string_revers.asm
+++ string_revers.asm
@@ -1,38 +1,49 @@
+INCLUDE 'EMU8086.INC'
 .MODEL SMALL
 .STACK 100H
 .DATA
-X DB 80 DUP ?
+STR DB 80 DUP ?
 .CODE
-
-    MAIN PROC
-        MOV AX,@DATA
-        MOV DS,AX
-        MOV SI,0
-        MOV AH,1
-        
-    NEXT:INT 21H
-        CMP AL,0DH
-        JZ EXIT
-        MOV X[SI],AL
-        INC SI
-        JMP NEXT
-    EXIT:DEC SI
+MAIN PROC
+    PRINT "ENTER THE STRING :"
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    MOV SI,0
+    
+    INPUT:
+    MOV AH,1
+    INT 21H
+    CMP AL,0DH
+    JE ENDINPUT
+    
+    MOV STR[SI],AL
+    INC SI
+    JMP INPUT
+    
+    ENDINPUT:
+    DEC SI
+    
     
     MOV AH,2
-    MOV DL,0DH
+    MOV DL,0AH
     INT 21H
     MOV DL,0DH
+    INT 21H   
+    
+    PRINT 'RESULT :'
+    OUTPUT:
+    MOV AH,2
+    MOV DL,STR[SI]
+    INT 21H
+    DEC SI
+    CMP SI,-2
+    JE ENDOUTPUT
+    JMP OUTPUT
+    
+    ENDOUTPUT:
+    MOV AH,4CH
     INT 21H
     
-    NEXTP: INT 21H
-        MOV DL,X[SI]
-        DEC SI
-        CMP SI,-2
-        JZ EXITP
-        JMP NEXTP
-    EXITP: 
-        MOV AH,4CH
-        INT 21H
-        MAIN ENDP
-    END MAIN
-    
+    MAIN ENDP
+END MAIN
